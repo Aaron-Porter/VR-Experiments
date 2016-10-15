a7556daf16634e6d1382e0b3bb3c5be8e3d0642e
Aaron-Porter <aaron@mnty.co>
2016-10-15 19:19:25 -0400

    working zero-g room

---
+++ Assets/Scripts/gravityAttractor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravityAttractor : MonoBehaviour {
+
+    public float pullRadius = 2;
+    public float pullForce = 1;
+    public bool enableAttractBool = false;
+
+    public void enableAttract(){
+    	enableAttractBool = true;
+    }
+
+    public void disableAttract(){
+    	enableAttractBool = false;
+    }
+
+ 
+	public void FixedUpdate() {
+		if(enableAttractBool == true){
+			attract();
+		}
+	}
+
+	public void attract(){
+		foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius)) {
+			if(collider.GetComponent<Rigidbody>() != null){
+			// calculate direction from target to me
+			Vector3 forceDirection = transform.position - collider.transform.position;
+
+			// apply force on target towards me
+			collider.GetComponent<Rigidbody>().AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+			}
+		}
+	}
+}

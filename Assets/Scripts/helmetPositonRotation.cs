41c29833b7e73d12132cd1498ca6fbdc06192008
Aaron-Porter <aaron@mnty.co>
2016-10-19 05:08:58 -0400

    spring for helmet

--- Assets/Scripts/helmetPositonRotation.cs
+++ Assets/Scripts/helmetPositonRotation.cs
@@ -4,14 +4,32 @@ using System.Collections;
 public class helmetPositonRotation : MonoBehaviour {
 
 	Vector3 rotationVector;
+	Vector3 headPosition;
+	Quaternion headRotation;
+	GameObject head;
+
+
+	void Awake(){
+		head = GameObject.Find("Camera (eye)");
+	}
 
 	void Update(){
+
+
+		// rotation
+		headRotation = head.transform.rotation;
+		transform.rotation = headRotation;
 		rotationVector = transform.rotation.eulerAngles;
 	}
 
-	void LateUpdate(){
-		rotationVector.x = 0;
-		transform.rotation = Quaternion.Euler(rotationVector);
-	}
+	// void LateUpdate(){
+	// 	// rotationVector.x = head.transform.rotation.x;
+	// 	// transform.rotation = Quaternion.Euler(rotationVector);
+	// }
+
+ //    void FixedUpdate() {
+ //        Vector3 turn = head.transform.up;
+ //        Vector3.RotateTowards(Vector3.up, turn)(turn);
+ //    }
 
 }

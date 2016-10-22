8064b0f853df7f2f9a02aca70052f2be4c1aeec7
Aaron-Porter <aaron@mnty.co>
2016-10-22 04:28:24 -0400

    Semi-working playspace orienting script

---
+++ Assets/Scripts/Centrifuge.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Centrifuge : MonoBehaviour {
+
+	public Vector3 eulerAngleVelocity;
+    Rigidbody rb;
+    public List<GameObject> objects;
+    public float gravitationalPull;
+ 
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+
+        Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime); // degrees a second - RPS
+        rb.MoveRotation(rb.rotation * deltaRotation); // add force to rotate
+
+        //or apply gravity to all game objects with rigidbody
+        foreach (GameObject o in UnityEngine.Object.FindObjectsOfType<GameObject>()) {
+            if(o.GetComponent<Rigidbody>() && o != this){
+                o.GetComponent<Rigidbody>().AddForce((this.transform.position - o.transform.position).normalized * gravitationalPull);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/stickToGround.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class stickToGround : MonoBehaviour {
+
+	GameObject head;
+	GameObject playspace;
+	List<Vector3> rayNormals = new List<Vector3>();
+	float headHeight;
+	RaycastHit hit;
+	Vector3 targetLocation;
+
+
+
+	void Awake(){
+		head = GameObject.Find("Camera (eye)");
+		playspace = GameObject.Find("[CameraRig]");
+	}
+
+	void Update(){
+		headHeight = head.transform.localPosition.y;
+
+		// cast rays
+		for(int i = 0; i < 3; i++){
+
+			var seperation = (float)0.5;
+			var offset = i * seperation;
+			var vectorPosition = head.transform.position;
+				vectorPosition.z = (vectorPosition.z - seperation) + offset;
+			var rayPosition = vectorPosition;
+
+			if (Physics.Raycast(rayPosition, Vector3.down, out hit)) {
+				Debug.DrawLine (transform.position, hit.point, Color.cyan);
+				rayNormals.Add(hit.normal);
+			};
+
+		}
+
+		var averagedNormals = (rayNormals[0] + rayNormals[1] + rayNormals[2]) / 3;
+
+		rotatePlayspace(averagedNormals);
+	}
+
+	void rotatePlayspace(Vector3 normal){
+
+	    // Quaternion targetRotation = Quaternion.FromToRotation(playspace.transform.up, normal);
+     //    Quaternion finalRotation = Quaternion.RotateTowards(playspace.transform.rotation, targetRotation, Time.deltaTime);
+
+     //    playspace.transform.rotation = finalRotation;
+
+	    var targetRotation = Quaternion.FromToRotation(playspace.transform.up, hit.normal) * playspace.transform.rotation;
+	    playspace.transform.rotation = Quaternion.Slerp(playspace.transform.rotation, targetRotation, Time.deltaTime * 1000);
+	}
+
+
+}

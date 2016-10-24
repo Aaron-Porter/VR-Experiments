df46d34ab8a9e08f7e6b04893594eacf3b2e1818
Aaron-Porter <aaron@mnty.co>
2016-10-24 03:31:35 -0400

    working teleportation

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using VRTK;
+
+public class Teleporter : MonoBehaviour {
+
+	VRTK_ControllerEvents controllerEvents;
+	LineRenderer lineRenderer;
+	public Color color;
+	public float width;
+	public bool rayVisible;
+	RaycastHit teleportPoint;
+	GameObject playspace;
+
+	void Start(){
+		playspace = GameObject.Find("[CameraRig]");
+		lineRenderer = gameObject.AddComponent<LineRenderer>() as LineRenderer;
+		lineRenderer.SetColors(color, color);
+		lineRenderer.SetVertexCount(2);
+	}
+
+	void Update(){
+
+		if(rayVisible == true){
+			Ray();
+		}
+
+	}
+
+	public void castRay(bool value){
+		rayVisible = value;
+		if(value == false){
+			lineRenderer.SetWidth(0,0);
+		}else{
+			lineRenderer.SetWidth(width,width);
+		}
+	}
+
+	public void Ray () {
+
+		var origin = transform.position;
+		var direction = transform.TransformDirection(Vector3.forward);
+		var endPoint = origin + direction * 1000000;
+        RaycastHit hit;
+
+        lineRenderer.SetPosition(0, origin);
+
+        if (Physics.Raycast(origin, direction, out hit) && hit.collider.gameObject.name == "walkingPath"){
+        	
+        	endPoint = hit.point;
+
+        	// Debug.DrawRay(origin, direction, Color.green);
+			lineRenderer.SetPosition(1, endPoint);
+			teleportPoint = hit;
+
+        }
+
+    }
+
+    public void teleport(){
+    	playspace.GetComponent<stickToGround>().rotatePlayspace(teleportPoint.normal);
+    	playspace.transform.position = teleportPoint.point;
+
+    }
+
+}
--- Assets/Scripts/stickToGround.cs
+++ Assets/Scripts/stickToGround.cs
@@ -18,39 +18,32 @@ public class stickToGround : MonoBehaviour {
 	}
 
 	void Update(){
-		headHeight = head.transform.localPosition.y;
+		// headHeight = head.transform.localPosition.y;
 
-		var rayPosition = head.transform.position;
-			rayPosition.y = rayPosition.y - 100;
+		// var rayPosition = head.transform.position;
+		// 	// rayPosition.y = rayPosition.y - 100;
 
-		if (Physics.Raycast(rayPosition, Vector3.up, out hit)) {
-			if(hit.collider.gameObject.name == "walkingPath"){
-				Debug.DrawLine (transform.position, hit.point, Color.cyan);
+		// if (Physics.Raycast(rayPosition, -Vector3.up, out hit)) {
+		// 	if(hit.collider.gameObject.name == "walkingPath"){
+		// 		Debug.DrawLine (transform.position, hit.point, Color.cyan);
 				
-				var normal = hit.normal;
-					normal.y = -normal.y;
-					normal.x = -normal.x;
-					normal.z = -normal.z;
-				Debug.Log(normal);
-				rotatePlayspace(normal);
-			}
-		};
-
-
-
-
-
+		// 		var normal = hit.normal;
+		// 		// 	normal.y = -normal.y;
+		// 		// 	normal.x = -normal.x;
+		// 		// 	normal.z = -normal.z;
+		// 		// Debug.Log(normal);
+		// 		rotatePlayspace(normal);
+		// 	}
+		// };
 	}
 
-	void rotatePlayspace(Vector3 normal){
+	public void rotatePlayspace(Vector3 normal){
 
 	    // Quaternion targetRotation = Quaternion.FromToRotation(playspace.transform.up, normal);
      //    Quaternion finalRotation = Quaternion.RotateTowards(playspace.transform.rotation, targetRotation, Time.deltaTime);
 
      //    playspace.transform.rotation = finalRotation;
-
-	    var targetRotation = Quaternion.FromToRotation(playspace.transform.up, normal) * playspace.transform.rotation;
-	    playspace.transform.rotation = targetRotation;
+		playspace.transform.up = normal;
 	}
 
 

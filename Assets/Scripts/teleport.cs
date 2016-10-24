f42bf69fe0531769088d88469d071711f2866d10
Aaron-Porter <aaron@mnty.co>
2016-10-23 23:09:08 -0400

    importing new centrifuge and working teleportation

---
+++ Assets/Scripts/teleport.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class teleport : MonoBehaviour {
+
+	void Update () {
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, forward, out hit)){
+        	Debug.DrawRay(transform.position, forward, Color.green);
+        }
+    }
+
+}
